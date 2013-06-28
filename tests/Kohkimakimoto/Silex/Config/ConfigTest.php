c3a81db0793a4ce76e42e5d9af4db1429ad48d19
kohkimakimoto <kohki.makimoto@gmail.com>
2013-06-28 12:58:55 +0900

    Add empty tests.

---
+++ tests/Kohkimakimoto/Silex/Config/ConfigTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Test\Kohkimakimoto\Silex\Config;
+
+use Kohkimakimoto\Silex\Config;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+  public function testDefault()
+  {
+  }
+}
---
+++ tests/Kohkimakimoto/Silex/Provider/ApplicationConfigServiceProviderTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace Test\Kohkimakimoto\Silex\Provider;
+
+use Kohkimakimoto\Silex\Provider\ApplicationConfigServiceProvider;
+
+class ApplicationConfigServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+  public function testDefault()
+  {
+  }
+}
